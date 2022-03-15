aaff254f27ab630736e24e09e36608a45c62acf8
AbigailSotomayor <abigail.sotomayor@hotmail.com>
2022-03-16 00:00:01 +0100

    Operation changed

--- Calculator/src/calculator/operations/Subtraction.java
+++ Calculator/src/calculator/operations/Subtraction.java
@@ -1,5 +1,24 @@
 package calculator.operations;
 
-public class Subtraction {
+public class Subtraction extends Operation {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "subtraction";
+	}
+
+	@Override
+	public String getOperator() {
+		// TODO Auto-generated method stub
+		return "-";
+	}
+
+	@Override
+	public double calc() {
+		// TODO Auto-generated method stub
+		return a - b;
+	}
+	
 
 }
